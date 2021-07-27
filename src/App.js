08a6ce30b8425fd3acdfc397fb0271e72dbe105f
sujal <php2.webmob@gmail.com>
2021-07-27 18:24:55 +0530

    call random API, on change filter the values, on click set the value in input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [users, setUsers] = useState([])
+    const [key, setKey] = useState('')
+    const [suggestion, setSuggestion] = useState([]);
+
+    useEffect(() => {
+        const loadUsers = async() => {
+            await axios.get('https://reqres.in/api/users')
+                .then((res) => {
+                    setUsers(res.data.data);
+                })
+        }
+        loadUsers();
+    }, [])
+
+    const onChangeHandler = (key) => {
+        let matches = [];
+        if(key.length > 0){
+            matches = users.filter(user => {
+                const regex = new RegExp(`${key}`, 'gi')
+                return user.email.match(regex)
+            })
+        }
+        setSuggestion(matches);
+        setKey(key);
+    }
+
+    const onClickHandler = (value) => {
+        setKey(value);
+        setSuggestion([]);
+    }
+
+    return (
+        <div className="container">
+            <input type="text" className="col-md-12 input"
+                   onChange={event => onChangeHandler(event.target.value)}
+                   style={{marginTop:'10px'}}
+                   value={key}
+                   onBlur={() => {
+                       setTimeout(() => {
+                           setSuggestion([])
+                       }, 100)
+                   }}
+            />
+            { suggestion && suggestion.map((user, index) =>
+                <div onClick={() => onClickHandler(user.email)} key={index} className="col-md-12" style={{cursor:"pointer"}}>{user.email}</div>
+            )}
+        </div>
+    );
 }
 
 export default App;
